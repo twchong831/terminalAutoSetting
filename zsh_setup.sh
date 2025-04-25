be610f9cca91a3c0fc56f5747ebb88ff9aa3b010
twwt <choungtw@gmail.com>
2025-04-25 15:58:18 +0900

    Update zsh_setup.sh

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,44 +1,18 @@
 #!/bin/zsh
 
-# 플러그인 설치
-echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+add_plugin_to_zshrc() {
+  local plugin_name=$1
+  if grep -q "^plugins=" ~/.zshrc; then
+    if ! grep "^plugins=" ~/.zshrc | grep -q "$plugin_name"; then
+      sed -i "s/^plugins=(\(.*\))/plugins=(\1 $plugin_name)/" ~/.zshrc
+      echo "✅ $plugin_name added to plugins in .zshrc"
+    fi
+  else
+    echo "plugins=($plugin_name)" >> ~/.zshrc
+    echo "✅ plugins=($plugin_name) added to .zshrc"
+  fi
+}
 
-echo "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# .zshrc에 플러그인 추가
-if ! grep -q "zsh-autosuggestions" ~/.zshrc; then
-  sed -i 's/^plugins=(/&zsh-autosuggestions /' ~/.zshrc
-fi
-
-if ! grep -q "zsh-syntax-highlighting" ~/.zshrc; then
-  sed -i 's/^plugins=(/&zsh-syntax-highlighting /' ~/.zshrc
-fi
-
-# powerlevel10k 설치 및 적용
-echo "Installing powerlevel10k..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-if ! grep -q "powerlevel10k.zsh-theme" ~/.zshrc; then
-  echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-fi
-
-# lsd 설치
-echo "Installing lsd via cargo..."
-sudo apt-get update
-sudo apt-get install -y cargo
-cargo install lsd
-
-echo "Linking lsd to /usr/bin..."
-sudo cp ~/.cargo/bin/lsd /usr/bin
-
-# alias 등록
-if ! grep -q "alias ls='lsd'" ~/.zshrc; then
-  echo "alias ls='lsd'" >> ~/.zshrc
-fi
-
-# 적용
-echo "Reloading zsh config..."
-source ~/.zshrc
-
-echo "✅ Zsh plugins, powerlevel10k, and lsd setup complete!"
+# 예: zsh-autosuggestions와 zsh-syntax-highlighting 추가
+add_plugin_to_zshrc zsh-autosuggestions
+add_plugin_to_zshrc zsh-syntax-highlighting
